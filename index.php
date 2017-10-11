d763d57e64ca19528e1d52f1091c2ad49260ed72
guiwestrup <westrupguilherme@gmail.com>
2017-10-11 17:19:55 -0300

    Primeiro commit, site já em funcionamento

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="SHORTCUT ICON" href="imagens/favicon.ico" type="image/x-icon" />    
+    <title>Rede OCP de Comunicação</title>
+    <link rel="alternate" type="application/xml" title="" href="" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link href="css/dashboard.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">    
+
+</head>
+<body>
+    <center>
+    <div class="header">
+        <a href=""><img class="img-responsive" src="imagens/logo-ocpnews.png" alt"Logo Grupo OCP de comunicação" title="Rede OCP de Comunicação"></a>
+    </div>
+    </center>
+    <center>
+    <div class="conectado">
+        Você já está conectado.
+    </div>
+    </center>
+    <div class="container">
+        <div class="container-fluid-center">
+            <main class="pt-2" role="main">
+                    <section class="row text-center placeholders">
+                        <div class="col-md-2 col-md-3 placeholder">
+                        <a href="https://ocponline.com.br/jornal-digital/"><img src="imagens/ocorreio.png" width="200" height="200" class="img-fluid rounded-circle" alt="Link para o Jornal Digital"></a>
+                        </div>
+                        <div class="col-md-2 col-md-3 placeholder">
+                        <a href="https://ocponline.com.br/"><img src="imagens/ocponline.png" width="200" height="200" class="img-fluid rounded-circle" alt="Link site O Correio do Povo"></a>
+                        </div>
+                        <div class="col-md-2 col-md-3 placeholder">
+                        <a href="https://poracaso.com/"><img src="imagens/poracaso.png" width="200" height="200" class="img-fluid rounded-circle" alt="Link para Site Por acaso"></a>
+                        </div>
+                        <div class="col-md-2 col-md-3 placeholder">
+                        <a href="https://www.aconteceuemjaragua.com.br/"><img src="imagens/aconteceu.png" width="200" height="200" class="img-fluid rounded-circle" alt="Link para site Aconteceu em Jaraguá"></a>
+                        </div>
+                    </section>
+            </main>
+        </div>
+    </div>
+        <footer>
+            <center><a href="#" class="links quem">quem somos</a> <a href="#" class="links plataforma" >plataforma</a> <a href="#" class="links anuncie" >anuncie conosco</a> <a href="#" class="links contato">contato</a></center>
+        </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/ocp.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+ </body>
+</html>
